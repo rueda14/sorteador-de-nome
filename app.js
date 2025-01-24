5597c6678e1ebdd9b8ebcdbc40692c295bbb2075
rueda14 <diegogrueda@gmail.com>
2025-01-24 01:15:37 -0300

    function adicionarAmigo() e limparCampo()

--- app.js
+++ app.js
@@ -2,4 +2,33 @@
 // habilidades em lógica de programação. 
 // Aqui você deverá desenvolver a lógica para resolver o problema.
 
+let amigos = [];
+
+
+function adicionarAmigo(){
+
+    let addAmigo = document.querySelector('input').value;
+    //console.log(addAmigo);
+    
+    if(addAmigo != ""){
+        if (amigos.includes(addAmigo)){
+            limparCampo();
+            return alert("Este nome já existe. Digite um nome diferente.");
+        }else{
+            amigos.push(addAmigo);
+            console.log(amigos);
+            limparCampo();
+            return addAmigo;
+        }
+    }else{
+        limparCampo();
+        return alert("Por favor, insira um nome");
+    }
+
+};
+
+function limparCampo(){
+  let addAmigo = document.querySelector('input');
+  addAmigo.value = '';
+};
 
