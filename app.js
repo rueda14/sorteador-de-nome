9f728ed40f64cbf5cd1f2eebf81f4448d8e367e2
rueda14 <diegogrueda@gmail.com>
2025-01-28 20:57:52 -0300

    versao final

--- app.js
+++ app.js
@@ -17,18 +17,44 @@ function adicionarAmigo(){
         }else{
             amigos.push(addAmigo);
             console.log(amigos);
+            exibirAmigos();
             limparCampo();
-            return addAmigo;
         }
     }else{
         limparCampo();
         return alert("Por favor, insira um nome");
-    }
+    };
 
 };
+
+function exibirAmigos(){
+
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = ""; 
+
+    
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li'); 
+        li.textContent = amigos[i]; 
+        lista.appendChild(li); 
+    }
+}
 
 function limparCampo(){
   let addAmigo = document.querySelector('input');
   addAmigo.value = '';
 };
 
+function sortearAmigo(){
+
+    if(amigos.length == 0){
+        alert('Não há amigos na lista para sortear');
+        return;
+    }else{
+        let lista = Math.floor(Math.random() * amigos.length)
+        let nomeSorteado = amigos[lista];
+        let resultadoFinal = document.getElementById('resultado');
+        resultadoFinal.innerHTML = `O amigo sorteado é: ${nomeSorteado}`;
+    }
+
+}
